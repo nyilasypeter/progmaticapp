9f03319626eb9134637d6aae9253b64ac10a3238
Nyilasy Péter <nyilasypeter@gmail.com>
2020-10-13 21:33:12 +0200

    detailed logging of permission denial by SecManager

--- src/main/java/com/progmatic/progmappbe/config/MyPolicy.java
+++ src/main/java/com/progmatic/progmappbe/config/MyPolicy.java
@@ -2,17 +2,20 @@ package com.progmatic.progmappbe.config;
 
 
 import com.progmatic.progmappbe.helpers.sourceevaluator.MyClassloader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.security.*;
 import java.util.Map;
 
 public class MyPolicy extends Policy {
 
+    private static final Logger Log = LoggerFactory.getLogger(MyPolicy.class);
 
     @Override
     public boolean implies(ProtectionDomain domain, Permission permission) {
         if(domain.getClassLoader() instanceof MyClassloader){
-            System.out.println("permission denied!!!!!!!!!!!!!!!!");
+            Log.warn("Permission will be denied for: {}", permission.toString() );
             return false;
         }
         return true;
